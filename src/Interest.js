7acb22839c3e4054709cb1732ca26474f070d43f
aking27 <aking27@wisc.edu>
2019-11-01 20:46:24 -0500

    Update Interest.js

--- src/Interest.js
+++ src/Interest.js
@@ -7,24 +7,32 @@ class Interest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sectionList: []
+      likedKeywords: [],
+      dislikedKeywords: []
     }
-    this.callbackFunction = this.callbackFunction.bind(this);
+    this.onAddKeyword = this.onAddKeyword.bind(this);
+    this.deleteKeyword = this.deleteKeyword.bind(this);
   }
-  callbackFunction(childData, data2){
-      this.setState({sectionList: this.state.sectionList.concat(childData)});
-      this.props.callbackFunction(childData, data2);
+
+  onAddKeyword(childData){ // keyword that's liked
+    this.setState({likedKeywords: this.state.likedKeywords.concat(childData)});
+    this.props.interestCallback(childData);
+  }
+  deleteKeyword(childData){ //keyword that's disliked
+    this.setState({dislikedKeywords: this.state.dislikedKeywords.concat(childData)});
+    this.props.interestDislikeCallback(childData);
+
   }
   getAreas() {
     let courses = [];
 
     for(const course of Object.entries(this.props.data)) {
-      courses.push (<AreasOfInterest key={course[0]} data={course[1]} callbackFunction={this.callbackFunction}/> );
+      courses.push (<AreasOfInterest key={course[0]} data={course[1]} onAddKeyword={this.onAddKeyword}
+        deleteKeyword={this.deleteKeyword}/> );
     }
 
     return courses;
   }
-
   render() {
     return (
       <div style={{margin: '10px'}}>
