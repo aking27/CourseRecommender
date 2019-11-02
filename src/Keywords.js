74e216b7800747a363ffc1f29fa6d1ea61db7557
aking27 <aking27@wisc.edu>
2019-11-01 20:47:14 -0500

    Update Keywords.js

--- src/Keywords.js
+++ src/Keywords.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import CourseSections from './CourseSections';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Keywords extends React.Component {
   render() {
     return (
       <>
-        <h5>Keyword: {this.props.data}</h5>
+        <h5>{this.props.data}</h5>
       </>
     )
   }
