267df2fcfe6fc7ed58663f4325b28c3c692eec8d
aking27 <aking27@wisc.edu>
2019-11-01 20:47:35 -0500

    Update Course.js

--- src/Course.js
+++ src/Course.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck'
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import CourseSections from './CourseSections';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Interest from './Interest.js';
 
 class Course extends React.Component {
     constructor(props) {
@@ -15,48 +11,23 @@ class Course extends React.Component {
         sectionList: [],
         like: true,
       }
-      // this.changeColor = this.changeColor.bind(this);
-    }
-    buttonClick(){
-      // this.setState({like: false}); //change true -> false and vice versa
-      this.props.onAddCourse(this.props.data.name);
     }
   render() {
-      // let btnColor = this.props.buttonColor ? "success" : "dark";
       var btnColor = "success";
 
-      if(this.props.buttonColor == false){
+      if(this.props.buttonColor === false){
         btnColor = "dark";
-
       }
     return (
               <>
-
-{/*You could use style={{display: 'flex', flexDirection: 'row'}}
-on the CardDeck and then put style={{flex: 1}} on the card.
-
-On button click -- change the color to just the outline
-<button type="button" class="btn btn-outline-danger">Danger</button>
-A better way would be to do the inital state and set state (on click event)
-
-App -- do the algorithm in there, maybe send more information to app like keywords or something
-,
-this.setState({likeButton: "btn btn-secondary"})
-
-<Button variant="primary">Primary</Button>
-()=>this.changeColor()
-*/}
                       <Card style={{width: '100%', marginTop: '5px', marginBottom: '5px'}}>
                         <Card.Body>
                           <Card.Title>{this.props.data.name}</Card.Title>
                           <Card.Subtitle className="mb-2 text-muted">{this.props.data.number} - {this.getCredits()}</Card.Subtitle>
-                          <Button variant="success" style={{marginLeft: '5px', width: '30%'}} onClick={ ()=>this.props.onAddCourse(this.props.data.subject) }>Like</Button>
-                          <Button variant="secondary"  style={{marginLeft: '5px', width: '30%'}}>Neutral</Button>
-                          <Button variant="danger" onClick={()=>this.props.dislikeCourse(this.props.data.name)} style={{marginLeft: '5px', width: '30%'}}>Dislike</Button>
+                          <Button variant="success" style={{marginLeft: '5px', width: '48%'}} onClick={ ()=>this.props.onAddCourse(this.props.data.subject) }>Like</Button>
+                          <Button variant="danger" onClick={()=>this.props.dislikeCourse(this.props.data.name)} style={{marginLeft: '5px', width: '48%'}}>Dislike</Button>
                         </Card.Body>
                       </Card>
-
-
         </>
     )
   }
