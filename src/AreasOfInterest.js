d6f36341fffdb5212401040bb4631012c10a0a87
aking27 <aking27@wisc.edu>
2019-11-01 20:46:47 -0500

    Update AreasOfInterest.js

--- src/AreasOfInterest.js
+++ src/AreasOfInterest.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Interest from './Interest';
 import Card from 'react-bootstrap/Card';
 import Keywords from './Keywords';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +11,7 @@ class AreasOfInterest extends React.Component {
     let keywords = [];
 
     for(const keyword of Object.entries(this.props.data.keywords)) {
-      keywords.push (<Keywords key={keyword[0]} data={keyword[1]} callbackFunction={this.callbackFunction}/> );
+      keywords.push (<Keywords key={keyword[0]} data={keyword[1]}/> );
     }
 
     return keywords;
@@ -24,9 +23,8 @@ class AreasOfInterest extends React.Component {
           <Card.Body>
             <Card.Title>{this.getKeywords()}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">Rate this group of keywords:</Card.Subtitle>
-            <button type="button" class="btn btn-success" style={{width: '30%'}}>Like</button> {/*on click want to send data*/}
-            <button type="button" class="btn btn-secondary" style={{width: '30%'}}>Neutral</button>
-            <button type="button" class="btn btn-danger" style={{marginLeft: '5px', width:'30%'}}>Dislike</button>
+            <button type="button" className="btn btn-success" style={{width: '48%'}} onClick={ ()=>this.props.onAddKeyword(this.props.data.name) }>Like</button>
+            <button type="button" className="btn btn-danger"  onClick={()=>this.props.deleteKeyword(this.props.data.name)} style={{marginLeft: '5px', width:'48%'}}>Dislike</button>
           </Card.Body>
         </Card>
       </>
