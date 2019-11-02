8d09b9d9db006ad75ca2c2c6aea681da4b35947a
aking27 <aking27@wisc.edu>
2019-11-01 20:47:53 -0500

    Update CourseArea.js

--- src/CourseArea.js
+++ src/CourseArea.js
@@ -9,34 +9,28 @@ class CourseArea extends React.Component {
     this.state = {
       sectionList: [],
       dislikeList: [],
-      buttonColor: true
+      buttonColor: true,
     }
-    //this.callbackFunction = this.callbackFunction.bind(this);
      this.onAddCourse = this.onAddCourse.bind(this);
      this.dislikeCourse = this.dislikeCourse.bind(this);
   }
   onAddCourse(childData){ // course that's liked
     this.setState({sectionList: this.state.sectionList.concat(childData)});
     this.props.callbackFunction(childData);
-    console.log(this.state.buttonColor);
-    this.setState({buttonColor: !this.state.buttonColor});
-
-    // console.log("childData: " + childData + " " + "sectionList length: " + this.state.sectionList.length);
+    console.log("like: " + childData);
   }
   dislikeCourse(childData){ // course that's disliked
     this.setState({dislikeList: this.state.dislikeList.concat(childData)});
     this.props.dislikeCallback(childData);
+    console.log("dislike: " + childData);
   }
-  // callbackFunction(childData, data2){
-  //     this.setState({sectionList: this.state.sectionList.concat(childData)});
-  //     this.props.callbackFunction(childData, data2);
-  // }
+
   getCourses() {
     let courses = [];
 
     for(const course of Object.entries(this.props.data)) {
-      courses.push (<Course key={course[0]} data={course[1]} callbackFunction={this.callbackFunction} onAddCourse={this.onAddCourse}
-        dislikeCourse={this.dislikeCourse} buttonColor={this.state.buttonColor} interest={this.props.data}/> );
+      courses.push (<Course key={course[0]} data={course[1]} onAddCourse={this.onAddCourse}
+        dislikeCourse={this.dislikeCourse}/> );
     }
 
     return courses;
